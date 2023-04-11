7cf555d727f32e330ea764d9ad86e919c5224758
AdamRepasky <adamson2326@gmail.com>
2023-04-11 23:50:23 +0200

    added cross-origins for port 9000

--- src/main/java/com/samply/feedbackagent/controller/SpecimenFeedbackController.java
+++ src/main/java/com/samply/feedbackagent/controller/SpecimenFeedbackController.java
@@ -15,23 +15,27 @@ public class SpecimenFeedbackController {
     SpecimenFeedbackRepository specimenFeedbackRepository;
 
     // Get All SpecimenFeedbacks
+    @CrossOrigin(origins = "http://localhost:9000")
     @GetMapping("/specimen-feedback")
     public List<SpecimenFeedback> getAllSpecimenFeedback() {
         return specimenFeedbackRepository.findAll();
     }
 
     // Create a new SpecimenFeedback
+    @CrossOrigin(origins = "http://localhost:9000")
     @PostMapping("/specimen-feedback")
     public SpecimenFeedback createSpecimenFeedback(@Valid @RequestBody SpecimenFeedback specimenFeedback) {
         return specimenFeedbackRepository.save(specimenFeedback);
     }
     // Create multiple SpecimenFeedback
+    @CrossOrigin(origins = "http://localhost:9000")
     @PostMapping("/multiple-specimen-feedback")
     public List<SpecimenFeedback> createSpecimenFeedback(@Valid @RequestBody SpecimenFeedbackDto specimenFeedbackDto) {
         return specimenFeedbackRepository.saveAll(specimenFeedbackDto.getFeedbackList());
     }
 
     // Get a Single SpecimenFeedback
+    @CrossOrigin(origins = "http://localhost:9000")
     @GetMapping("/specimen-feedback/{id}")
     public SpecimenFeedback getSpecimenFeedbackById(@PathVariable(value = "id") Long specimenFeedbackId) throws SpecimenFeedbackNotFoundException {
         return specimenFeedbackRepository.findById(specimenFeedbackId)
@@ -39,12 +43,14 @@ public class SpecimenFeedbackController {
     }
 
     // Get SpecimenFeedbacks by request ID
+    @CrossOrigin(origins = "http://localhost:9000")
     @GetMapping("/specimen-feedback/find-by-request/{id}")
     public List<SpecimenFeedback> getSpecimenFeedbackByRequestID(@PathVariable(value = "id") Long requestId) {
         return specimenFeedbackRepository.findByRequest(requestId);
     }
 
     // Update a SpecimenFeedback
+    @CrossOrigin(origins = "http://localhost:9000")
     @PutMapping("/specimen-feedback/{id}")
     public SpecimenFeedback updateSpecimenFeedback(@PathVariable(value = "id") Long specimenFeedbackId,
                            @Valid @RequestBody SpecimenFeedback specimenFeedbackDetails) throws SpecimenFeedbackNotFoundException {
@@ -60,6 +66,7 @@ public class SpecimenFeedbackController {
         return specimenFeedbackRepository.save(specimenFeedback);
     }
     // Update a SpecimenFeedback
+    @CrossOrigin(origins = "http://localhost:9000")
     @PutMapping("/specimen-feedback/add-publication/{id}")
     public SpecimenFeedback addPublicationReferenceSpecimenFeedback(@PathVariable(value = "id") Long specimenFeedbackId,
                                                    @Valid @RequestBody SpecimenFeedback specimenFeedbackDetails) throws SpecimenFeedbackNotFoundException {
@@ -73,6 +80,7 @@ public class SpecimenFeedbackController {
     }
 
     // Delete a SpecimenFeedback
+    @CrossOrigin(origins = "http://localhost:9000")
     @DeleteMapping("/specimen-feedback/{id}")
     public ResponseEntity<?> deleteSpecimenFeedback(@PathVariable(value = "id") Long specimenFeedbackId) throws SpecimenFeedbackNotFoundException {
         SpecimenFeedback specimenFeedback = specimenFeedbackRepository.findById(specimenFeedbackId)
